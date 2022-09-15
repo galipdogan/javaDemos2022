28875b01de8de98d032eae7c82813955ab5b5f0e
Galip DOĞAN <80524618+galipdogan@users.noreply.github.com>
2022-09-15 23:47:52 +0300

    Add files via upload
    JavaCamp2022

---
+++ variables/src/variables/Main.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ogrenciSayisi =12;
+		String mesaj="Ogrenci sayisi : ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Ogrenci sayim : 9");
+		System.out.println("Ogrenci sayim : 9");
+		System.out.println("Ogrenci sayim : 9");
+		System.out.println("Ogrenci sayim : 9");
+	}
+
+}
