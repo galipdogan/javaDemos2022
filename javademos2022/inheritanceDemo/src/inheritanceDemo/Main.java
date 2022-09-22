9b0cb21636090596bb7450297ba0c8112572bedf
Galip <Galip@LAPTOP-V7VQ7B50>
2022-09-22 18:39:24 +0300

    Lesson2 javacamp

---
+++ javademos2022/inheritanceDemo/src/inheritanceDemo/Main.java
@@ -0,0 +1,14 @@
+package inheritanceDemo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		OgretmenKrediManager ogretmenKrediManager=new OgretmenKrediManager();
+		TarimKrediManager tarimKrediManager=new TarimKrediManager();
+		
+		KrediUI krediUI=new KrediUI();
+		krediUI.KrediHesapla(new OgretmenKrediManager());
+	}
+
+}
